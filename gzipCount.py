da207e6e2516e6ba3bf98fcc9ff945296ae12444
tchanOICR <tchan@oicr.on.ca>
2021-09-27 09:12:19 -0400

    Update gzipCount.py
    Updated file writing format, removed empty cycles, improved code formatting, added new function specifically as a ".fastq.gz"-file reader

--- gzipCount.py
+++ gzipCount.py
@@ -5,29 +5,87 @@ Created on Fri Aug 27 12:45:41 2021
 @author: tchan
 """
 
-import json, csv, gzip, sys
-from datetime import date
+def file_reader(fileName,origFile):
+    import gzip
+    from datetime import date
+    
+    cycles = []
+    newCycles = []
+    totalCount = {'A':0,'T':0,'C':0,'G':0,'N':0}
+    maxLen = 0
+    
+    if isinstance(fileName,list) == False:
+        loose_file_list = []
+        loose_file_list.append(fileName)
+        fileName = loose_file_list
+    
+    for Input in fileName:
+        with gzip.open(Input,'rt') as current_file:
+            line = 1
+            for record in current_file:
+                if line == 1 or line == 3:
+                    line += 1
+                elif line == 2:
+                    if 'R1.fastq' in Input:
+                        count = 0
+                    elif 'R2.fastq' in Input:
+                        count = len(record)-1
+                    testLen = len(record)-1
+                    if testLen > maxLen:
+                        if cycles == []:
+                            for n in range(testLen*2):
+                                cycles.append({'A':0,'T':0,'C':0,'G':0,'N':0})
+                        else:
+                            for n in range((testLen-maxLen)*2):
+                                cycles.append({'A':0,'T':0,'C':0,'G':0,'N':0})
+                        maxLen = testLen
+                    for char in record[:-1]:
+                        if char == 'A':
+                            cycles[count]['A'] += 1
+                        elif char == 'T':
+                            cycles[count]['T'] += 1
+                        elif char == 'C':
+                            cycles[count]['C'] += 1
+                        elif char == 'G':
+                            cycles[count]['G'] += 1
+                        elif char == 'N':
+                            cycles[count]['N'] += 1
+                        count += 1
+                    line += 1
+                elif line == 4:
+                    line = 1
+                
+    for cycle in cycles:
+        if cycle == {'A':0,'T':0,'C':0,'G':0,'N':0}:
+            continue
+        cycle = {key:val for key, val in cycle.items() if val != 0}
+        newCycles.append(cycle)
+        for key in cycle:
+            totalCount[key] += cycle[key]
+    
+    date = str(date.today())
+    textFile = open(date+' '+origFile+'.txt','w')
+    textFile.write(str(newCycles))
+    textFile.write('\n'+str(totalCount))
+    textFile.close()
+    print('File created.')    
 
-cycles = []
-newCycles = []
-foundFiles = []
-totalCount = {'A':0,'T':0,'C':0,'G':0,'N':0}
-maxLen = 0
-
-def Few_files(foundFiles,fileName):
-    foundFiles.append(fileName)
-    return foundFiles
-
-def CSV_reader(foundFiles,fileName):
+def CSV_reader(fileName):
+    import csv
+    
+    fastqFiles = []
     with open(fileName) as csv_file:
         file = csv.reader(csv_file, delimiter = ',')
         for row in file:
             if '.fastq.gz' in row[5] and '.fastq.gz' in row[6]:
-                foundFiles.append(row[5])
-                foundFiles.append(row[6])
-    return foundFiles
+                fastqFiles.append(row[5])
+                fastqFiles.append(row[6])
+    file_reader(fastqFiles,fileName)
 
-def JSON_reader(foundFiles,fileName):
+def JSON_reader(fileName):
+    import json
+    
+    fastqFiles = []
     with open(fileName) as json_file:
         data = json.loads(json_file.read())
         type(data)
@@ -35,61 +93,17 @@ def JSON_reader(foundFiles,fileName):
             for key1 in list_value:
                 if key1 == 'files':
                     for key2 in list_value['files']:
-                        foundFiles.append(key2['path'])
-    return foundFiles
+                        fastqFiles.append(key2['path'])
+    file_reader(fastqFiles,fileName)
+
+import sys
+
+looseFiles = []
 
 for file_type in sys.argv:
     if '.fastq.gz' in file_type:
-        Few_files(foundFiles,file_type)
+        file_reader(file_type,file_type)
     elif '.csv' in file_type:
-        CSV_reader(foundFiles,file_type)
+        CSV_reader(file_type)
     elif '.json' in file_type:
-        JSON_reader(foundFiles,file_type)
-
-for Input in foundFiles:
-    with gzip.open(Input,'rt') as current_file:
-        line = 1
-        for record in current_file:
-            if 'R1.fastq' in Input:
-                count = 0
-            elif 'R2.fastq' in Input and line == 2:
-                count = len(record)-1
-            if line == 1 or line == 3:
-                line += 1
-            elif line == 2:
-                testLen = len(record)-1
-                if testLen > maxLen:
-                    if cycles == []:
-                        for n in range(testLen*2):
-                            cycles.append({'A':0,'T':0,'C':0,'G':0,'N':0})
-                    else:
-                        for n in range((testLen-maxLen)*2):
-                            cycles.append({'A':0,'T':0,'C':0,'G':0,'N':0})
-                    maxLen = testLen
-                for char in record[:-1]:
-                    if char == 'A':
-                        cycles[count]['A'] += 1
-                    elif char == 'T':
-                        cycles[count]['T'] += 1
-                    elif char == 'C':
-                        cycles[count]['C'] += 1
-                    elif char == 'G':
-                        cycles[count]['G'] += 1
-                    elif char == 'N':
-                        cycles[count]['N'] += 1
-                    count += 1
-                line += 1
-            elif line == 4:
-                line = 1
-                
-for cycle in cycles:
-    cycle = {key:val for key, val in cycle.items() if val != 0}
-    newCycles.append(cycle)
-    for key in cycle:
-        totalCount[key] += cycle[key]
-
-date = str(date.today())
-textFile = open(date+'.txt','w')
-textFile.write(str(totalCount))
-textFile.close()
-print('File created.')
+        JSON_reader(file_type)
